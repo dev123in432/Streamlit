2b71f1331b8076295f33754d549565f7e83db526
David I <45247943+dev123in432@users.noreply.github.com>
2023-05-19 02:07:50 +1000

    Update my-first-app.py

--- my-first-app.py
+++ my-first-app.py
@@ -2,4 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Uber pickups in NYC')
+st.title('my first app')
+
+x = st.slider('Select a value')
+st.write(x, 'squared is', x * x)
